eccccd03b18bd41ee88a9c8cd6979766803db857
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-08 19:45:11 +0700

    change

--- lab2_2.c
+++ lab2_2.c
@@ -2,14 +2,30 @@
 int main()
 {
     char pro;
-    float t,sum;
-    int s,m;
+    float t,sum,s,m;
     scanf("%c",&pro);
     scanf("%f",&t);
-    m= (int)t;
-    s= (t-m)*100.0;
-    printf("%d\n",s);
-    printf("%d\n",m);
+    if(pro == 'A')
+    {
+        sum = 199.00;
+        if(t>200.00)
+        {
+            s=(int)(t-200);
+            m=(t-200-s)*100;
+            sum = sum + ((s+(m/60))*3);
+        }
 
+    }
+    else if(pro == 'B')
+    {
+        sum = 299.00;
+        if(t>400.00)
+        {
+            s=(int)(t-400);
+            m=(t-400-s)*100;
+            sum = sum + ((s+(m/60))*2);
+        }
+    }
+    printf("%.2f",sum);
     return 0;
 }
