cbdad1b7fdad1cccfc30f1625fc8b1cf3a8ba2d9
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-08 18:03:06 +0700

    change

--- lab2_3.c
+++ lab2_3.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include <string.h>
 int main()
 {
-    int n[3],i,j,t,m=0,a,b,c;
-    char ap[3],as[3]={'C','B','A'};
-        scanf("%d %d %d",&n[0],&n[1],&n[2]);
-        scanf("%c%c%c",&ap[0],&ap[1],&ap[2]);
-     while(1)
+    int n[3],i,j,t=0;
+    char ap[3];
+    scanf("%d %d %d",&n[0],&n[1],&n[2]);
+    fflush(stdin);
+    scanf("%[^\n]s",ap);
+    while(1)
     {
         int k=0;
         for(i=0; i<2; i++)
@@ -23,8 +25,24 @@ int main()
             }
         }
         if(k==0)
-         break;
+            break;
     }
-
+    for(i=0; i<3; i++)
+        printf("%d ",n[i]);
+    for(i=0; i<3; i++)
+    {
+        switch(ap[i])
+        {
+        case 'A':
+            printf("%d ",n[0]);
+            break;
+        case 'B':
+            printf("%d ",n[1]);
+            break;
+        case 'C':
+            printf("%d ",n[2]);
+            break;
+        }
+    }
     return 0;
 }
