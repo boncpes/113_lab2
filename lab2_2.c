684b6ee70ab680186c19c0575b9de946fbb5b193
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-08 16:55:24 +0700

    change variable

--- lab2_2.c
+++ lab2_2.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int pro,t,sum;
+    char pro
+    float t,sum;
     scanf("%c",&pro);
+    scnaf("%f",&t);
     if(pro == 'A')
     {
-        sum = 199;
-        if(t>200)
+        sum = 199.00;
+        if(t>200.00)
         {
 
         }
@@ -14,13 +16,13 @@ int main()
     }
     else if(pro == 'B')
     {
-        sum = 299
-        if(t>400)
+        sum = 299.00;
+        if(t>400.00)
         {
 
         }
 
     }
-
+    printf("%d",sum);
     return 0;
 }
