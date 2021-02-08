2aaa482dd6d410e43c87a78b1390170a9aa9cb8e
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-08 18:12:15 +0700

    Initial for 4

---
+++ lab2_4.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    int d;
+    scanf("%c",&n);
+    scanf("%d",&d);
+    if(d%2 == 0)
+    {
+        if(n=>65 && n<74)
+        {
+            if(d%5==0)
+                printf("\(^_^)/")
+            else
+            printf("(^_^)");
+        }
+        else if(n=>74 && n<83)
+        {
+            if(d%5==0)
+                printf("\(*o*)/")
+            else
+            printf("(*o*)");
+        }
+        else if(n=>83 && n<100)
+        {
+            if(d%5==0)
+                printf("\(T_T)/")
+            else
+            printf("(T_T)");
+        }
+    }
+    else
+    {
+         if(n=>65 && n<74)
+        {
+            if(d%5==0)
+                printf("\(^_^)/")
+            else
+            printf("(^_^)");
+        }
+        else if(n=>74 && n<83)
+        {
+            if(d%5==0)
+                printf("\(*o*)/")
+            else
+            printf("(*o*)");
+        }
+        else if(n=>83 && n<100)
+        {
+            if(d%5==0)
+                printf("\(T_T)/")
+            else
+            printf("(T_T)");
+        }
+    }
+    return 0;
+}
