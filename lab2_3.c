a4defc0c516bc0ffc95135076723eae8ef99e90b
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-08 17:35:12 +0700

    new sort

--- lab2_3.c
+++ lab2_3.c
@@ -1,21 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int n[3],i,j,t,m,a,b,c;
-    for(i=0; i<3; i++)
-        scanf("%d",&n[i]);
-    for(i=1; i<3; i++)
+    int n[3],i,j,t,m=0,a,b,c;
+    char ap[3],as[3]={'C','B','A'};
+        scanf("%d %d %d",&n[0],&n[1],&n[2]);
+        scanf("%c%c%c",&ap[0],&ap[1],&ap[2]);
+     while(1)
     {
-        for(j=0; j<i; j++)
+        int k=0;
+        for(i=0; i<2; i++)
         {
-            if(n[j]>n[i])
+            if(n[i]>n[i+1])
+                k++;
+        }
+        for(i=0; i<2; i++)
+        {
+            if(n[i]>n[i+1])
             {
-                m=n[i]
-                  for(t=i; i>=j; t--)
-                      n[t]=n[t-1];
-                n[j]=m;
+                t=n[i];
+                n[i]=n[i+1];
+                n[i+1]=t;
             }
         }
+        if(k==0)
+         break;
     }
+
     return 0;
 }
